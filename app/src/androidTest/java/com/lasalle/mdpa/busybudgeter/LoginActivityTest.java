c076cecc6ba5f680db6c7a00bccf618c7041f2bf
Albert De Nova <albert.dnova@gmail.com>
2018-02-18 12:48:02 +0100

    Add simple test that will just launch stuff

--- app/src/androidTest/java/com/lasalle/mdpa/busybudgeter/LoginActivityTest.java
+++ app/src/androidTest/java/com/lasalle/mdpa/busybudgeter/LoginActivityTest.java
@@ -3,17 +3,36 @@ package com.lasalle.mdpa.busybudgeter;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.lasalle.mdpa.busybudgeter.view.LoginActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 @MediumTest
 public class LoginActivityTest {
 
-    // CODE GOES HERE
+    @Rule
+    public ActivityTestRule<LoginActivity> loginActivity = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void emptyUserAndPassword() {
+        onView(withId(R.id.username))
+                .perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.password))
+                .perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.login_button)).perform(click());
+    }
 
 }
