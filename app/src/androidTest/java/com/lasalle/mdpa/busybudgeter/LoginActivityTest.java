e2a39ae065a51e7ee063a12f0c963ed8d82776d3
Albert De Nova <albert.dnova@gmail.com>
2018-02-18 12:02:49 +0100

    Setup empty LoginActivityTest

---
+++ app/src/androidTest/java/com/lasalle/mdpa/busybudgeter/LoginActivityTest.java
@@ -0,0 +1,19 @@
+package com.lasalle.mdpa.busybudgeter;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.MediumTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class LoginActivityTest {
+
+    // CODE GOES HERE
+
+}
