86995832394679675b71d8c3b794074409785d77
Albert De Nova <albert.dnova@gmail.com>
2018-02-18 20:57:45 +0100

    Fix unit test when clicking bottom navigation options

--- app/src/androidTest/java/com/lasalle/mdpa/busybudgeter/ContainerActivityTest.java
+++ app/src/androidTest/java/com/lasalle/mdpa/busybudgeter/ContainerActivityTest.java
@@ -8,11 +8,15 @@ import android.support.v4.app.FragmentManager;
 
 import com.lasalle.mdpa.busybudgeter.view.ContainerActivity;
 import com.lasalle.mdpa.busybudgeter.view.HomeFragment;
+import com.lasalle.mdpa.busybudgeter.view.SettingsFragment;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
 
@@ -29,6 +33,16 @@ public class ContainerActivityTest {
         Fragment currentFragment = fragment.findFragmentById(R.id.fragment_container);
 
         assertThat(currentFragment, instanceOf(HomeFragment.class));
+    }
+
+    @Test
+    public void clickingSettingsOptionLoadsProperFragment() {
+        onView(withId(R.id.navigation_settings)).perform(click());
+
+        FragmentManager fragment = containerActivityRule.getActivity().getSupportFragmentManager();
+        Fragment currentFragment = fragment.findFragmentById(R.id.fragment_container);
+
+        assertThat(currentFragment, instanceOf(SettingsFragment.class));
     }
 
 }
--- app/src/main/java/com/lasalle/mdpa/busybudgeter/view/ContainerActivity.java
+++ app/src/main/java/com/lasalle/mdpa/busybudgeter/view/ContainerActivity.java
@@ -3,6 +3,7 @@ package com.lasalle.mdpa.busybudgeter.view;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
@@ -30,14 +31,23 @@ public class ContainerActivity extends AppCompatActivity  implements BottomNavig
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
         switch (item.getItemId()) {
             case R.id.navigation_home:
-
+                fragment = new HomeFragment();
+                break;
             case R.id.navigation_dashboard:
 
-            case R.id.navigation_notifications:
+            case R.id.navigation_settings:
+                fragment = new SettingsFragment();
+                break;
 
         }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+        item.setChecked(true);
 
         return true;
     }
